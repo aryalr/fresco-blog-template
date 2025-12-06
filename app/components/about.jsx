6a0fc82ffe8d256a3867d639786e2ee680622fbc
aryalr <aryajati45@gmail.com>
2025-12-07 03:33:42 +0700

    1. reset migration dan migrate ulang 2. menjalankan seeding ulang 3. melakukan fetching data dari endpoint API

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -13,12 +13,12 @@ export const About = (props) => {
             <div className="about-text">
               <h2>Tentang Kami</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>
+              <h3>Kenapa Pilih FresCo?</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
-                      ? props.data.Why.map((d, i) => (
+                      ? props.data.Why?.map((d, i) => (
                           <li key={`${d}-${i}`}>{d}</li>
                         ))
                       : "loading"}
@@ -27,7 +27,7 @@ export const About = (props) => {
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
-                      ? props.data.Why2.map((d, i) => (
+                      ? props.data.Why2?.map((d, i) => (
                           <li key={`${d}-${i}`}> {d}</li>
                         ))
                       : "loading"}
