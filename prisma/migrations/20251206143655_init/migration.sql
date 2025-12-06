1f9d2bd47e5d10ba64818723bcd69f25fdb86359
aryalr <aryajati45@gmail.com>
2025-12-06 21:37:22 +0700

    first database migration

---
+++ prisma/migrations/20251206143655_init/migration.sql
@@ -0,0 +1,56 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Header" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT DEFAULT 1,
+    "title" TEXT NOT NULL,
+    "paragraph" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "About" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT DEFAULT 1,
+    "paragraph" TEXT NOT NULL,
+    "why" TEXT NOT NULL,
+    "why2" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Gallery" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "largeImage" TEXT NOT NULL,
+    "smallImage" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Service" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "icon" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "text" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Testimonial" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "img" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "img" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "job" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
